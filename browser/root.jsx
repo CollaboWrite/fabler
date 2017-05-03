e607040309eeb03dd9625a2aa437a58a6f671bde
Yejee Arin Choi <yej.arin.choi@gmail.com>
2017-05-03 15:50:07 -0400

    can get sentiment from firebase db

--- browser/root.jsx
+++ browser/root.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import axios from 'axios'
 
+const sentimentDb = require('../server/db/seed')
+
 const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
 const SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
 const SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
 
-const phrasePara = document.querySelector('.phrase');
-const diagnosticPara = document.querySelector('.output');
-
-const testBtn = document.querySelector('button');
+// TODO: removed Disable button property - can add later
 
 let counter = -1
 function nextPhrase() {
@@ -21,7 +20,9 @@ export default class Root extends React.Component{
     super(props)
     this.state = {
       prompt: [ 'A very young Mouse, who had never seen anything of the world, almost came to grief the very first time he ventured out.',
-                'And this is the story he told his mother about his adventures.']
+                'And this is the story he told his mother about his adventures.'],
+      testBtn: '',
+      backgroundColor: 'white'
     }
     this.testSpeech = this.testSpeech.bind(this)
     this.handleClick = this.handleClick.bind(this)
@@ -31,16 +32,17 @@ export default class Root extends React.Component{
     this.testSpeech()
   }
   testSpeech() {
-    testBtn.disabled = true;
-    testBtn.textContent = 'Test in progress';
+    const self = this
+    this.testBtn = 'disabled'
+    // testBtn.textContent = 'Test in progress';
 
-    const phrase = phrases[nextPhrase()];
-    phrasePara.textContent = phrase;
+    // const phrase = phrases[nextPhrase()];
+    // phrasePara.textContent = phrase;
 
-    const grammar = '#JSGF V1.0; grammar phrase; public <phrase> = ' + phrase +';';
+    // const grammar = '#JSGF V1.0; grammar phrase; public <phrase> = ' + phrase +';';
     const recognition = new SpeechRecognition();
     const speechRecognitionList = new SpeechGrammarList();
-    speechRecognitionList.addFromString(grammar, 1);
+    // speechRecognitionList.addFromString(grammar, 1);
     recognition.grammars = speechRecognitionList;
     recognition.lang = 'en-US';
     recognition.interimResults = false;
@@ -59,36 +61,57 @@ export default class Root extends React.Component{
       // The second [0] returns the SpeechRecognitionAlternative at position 0.
       // We then return the transcript property of the SpeechRecognitionAlternative object
       const speechCompiler = event.results[0][0].transcript;
-      diagnosticPara.textContent = 'Speech received: ' + speechCompiler + '.';
+      // diagnosticPara.textContent = 'Speech received: ' + speechCompiler + '.';
 
       sentence = speechCompiler
 
-      console.log(speechCompiler)
+      // console.log(speechCompiler)
 
-      console.log('Confidence: ' + event.results[0][0].confidence);
+      // console.log('Confidence: ' + event.results[0][0].confidence);
     }
 
     recognition.onspeechend = function() {
       recognition.stop();
-      testBtn.disabled = false;
-      testBtn.textContent = 'Start new test';
+      this.testBtn = ''
+      // testBtn.textContent = 'Start new test';
       axios.post('/input', {sentence})
+      .then((nlpObj) => {
+        let sentiment = nlpObj.sentiment
+        if (sentiment < -0.75) {
+          sentiment = 'negative'
+        } else if (sentiment > 0.75) {
+          sentiment = 'positive'
+        } else {
+          sentiment = 'neutral'
+        }
+        let bgColor
+        sentimentDb.orderByChild('name').equalTo(sentiment).once('value')
+        .then(function(snapshot) {
+          bgColor = snapshot.val()
+          nlpObj.bgColor = bgColor[sentiment].color
+          return nlpObj
+        })
+        .then(function(nlpObj){
+          self.setState({backgroundColor: nlpObj.bgColor})
+        })
+        // return nlpObj    if we need to do further Firebase queries!!!
+      })
+      .catch(err => console.error(err))
     }
 
     recognition.onerror = function(event) {
-      testBtn.disabled = false;
-      testBtn.textContent = 'Start new test';
-      diagnosticPara.textContent = 'Error occurred in recognition: ' + event.error;
+      this.testBtn = ''
+      // testBtn.textContent = 'Start new test';
+      // diagnosticPara.textContent = 'Error occurred in recognition: ' + event.error;
     }
 
   }
 
   render() {
     return (
-      <div>
+      <div style={{backgroundColor: this.state.backgroundColor}}>
         <h1>Phrase matcher</h1>
         <p>Press the button then say the phrase to test the recognition.</p>
-
         <button onClick={this.handleClick}>Start new test</button>
         <div>
             <p className="phrase">Phrase...</p>
--- server/db/seed.js
+++ server/db/seed.js
@@ -5,8 +5,13 @@ const seedAction = require('./seedAction')
 const firebase = require('./index')
 
 const db = firebase.database()
+// const nounDb = db.ref('nouns')
+const sentimentDb = db.ref('sentiments')
+// const actionDb = db.ref('actions')
 
 // TO SEED, un-comment and run this file
 // const nounDb = db.ref('nouns').set(seedNoun)
 // const sentimentDb = db.ref('sentiments').set(seedSentiment)
 // const actionDb = db.ref('actions').set(seedAction)
+
+module.exports = sentimentDb
